9263cbe9bde1c30f263bc7d157c6648c688f88a8
Tony HENG <tony.heng@epita.fr>
2021-04-23 23:28:31 +0200

    set list item icon

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -7,17 +7,19 @@ export default function Checkout({pizzas, total}) {
   const checkoutStyle = "checkout-card col-3 shadow d-flex flex-column";
   const checkoutTitleStyle = "checkout-title";
   const checkoutPriceStyle = "checkout-price mt-auto mb-3";
+  const pizzaStyle = "checkout-pizza";
+  const toppingStyle = "checkout-topping";
   
 
   return <div className={checkoutStyle}>
     <div className={checkoutTitleStyle}>Checkout</div>
     <ul>
       {pizzas.map(pizza => 
-        <li key={pizza.id}>
-          Pizza: {pizza.size && pizza.size.size} {pizza.sauce} {pizza.cheese}
+        <li key={pizza.id} className={pizzaStyle}>
+          Pizza - {pizza.size && pizza.size.size} <span className="font-italic">{pizza.sauce}</span> {pizza.cheese}
           <ul>
             {pizza.toppings.map(topping =>
-              <li key={topping.name}>
+              <li key={topping.name} className={toppingStyle}>
                 {topping.name}
               </li>)}
           </ul>
