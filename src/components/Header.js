fd7057e0c77dbb8dba3dcb3b2822dca1f79090e6
Tony HENG <tony.heng@epita.fr>
2021-04-23 23:28:12 +0200

    add github logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import react from 'react'
 
-import '../css/Header.scss'
+import '../css/Header.scss';
+import Logo from '../assets/svg/pizza.svg';
+import Github from '../assets/svg/github.svg';
 
 export default function Header() {
   const headerStyle = "w-100 d-flex";
   const brandNameStyle = "text-white m-auto font-weight-bold";
+  const logoStyle = "logo ml-3";
+  const githubStyle = "github mr-3 d-flex";
+  const github = "https://github.com/TonyHg"
 
   // TODO: add logo + github
   return <div id="header" className={headerStyle}> 
+    <img className={logoStyle} src={Logo}/>
     <span className={brandNameStyle}>Pizza Creator</span>
+    <a href={github} target="_blank" className={githubStyle}><img src={Github}/></a>
   </div>
 }
