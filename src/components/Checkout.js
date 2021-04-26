1d32d0108928d5dca4fd798541cd98fda6418c25
Tony HENG <tony.heng@epita.fr>
2021-04-26 13:50:59 +0200

    fix overflow on long order

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,6 +6,7 @@ import '../css/Checkout.scss'
 export default function Checkout({pizzas, total}) {
   const checkoutStyle = "checkout-card col-3 shadow-lg d-flex flex-column";
   const checkoutTitleStyle = "checkout-title";
+  const checkoutListStyle = "checkout-list";
   const checkoutPriceStyle = "checkout-price mt-auto mb-3";
   const pizzaStyle = "checkout-pizza";
   const toppingStyle = "checkout-topping";
@@ -13,10 +14,10 @@ export default function Checkout({pizzas, total}) {
 
   return <div className={checkoutStyle}>
     <div className={checkoutTitleStyle}>Checkout</div>
-    <ul>
+    <ul className={checkoutListStyle}>
       {pizzas.map(pizza => 
         <li key={pizza.id} className={pizzaStyle}>
-          Pizza - {pizza.size && pizza.size.size} <span className="font-italic">{pizza.sauce}</span> {pizza.cheese}
+          Pizza - {pizza.size && pizza.size.size} <span className="font-italic">{pizza.sauce && pizza.sauce.name}</span> {pizza.cheese && pizza.cheese.name}
           <ul>
             {pizza.toppings.map(topping =>
               <li key={topping.name} className={toppingStyle}>
