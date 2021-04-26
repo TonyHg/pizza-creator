19fbb8246cfcff0c5f6558f48808a3685f3f34c5
Tony HENG <tony.heng@epita.fr>
2021-04-26 15:49:45 +0200

    add start screen

--- src/App.js
+++ src/App.js
@@ -19,10 +19,16 @@ const initPizza = {
 }
 
 function App() {
+  const [loading, setLoading] = useState(true)
   const [pizzas, setPizzas] = useState([deepCopyOf(initPizza)]);
   const [currentPizzaIdx, setCurrentPizzaIdx] = useState(0);
   const [currentPizza, setCurrentPizza] = useState(pizzas[currentPizzaIdx]);
-  const [modalVisibility, setModalVisibility] = useState(false)
+  const [modalVisibility, setModalVisibility] = useState(false);
+  const [startStyle, setStartStyle] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+  }, []);
 
   useEffect(() => {
     updateCurrentPizza();
@@ -82,10 +88,17 @@ function App() {
   const onPay = () => {
     resetPizzas();
     setModalVisibility(true);
-    setTimeout(() => setModalVisibility(false), 2000);
+    setTimeout(() => setModalVisibility(false), 1500);
+    setTimeout(() => setLoading(true), 2000);
+  }
+
+  const onStart = () => {
+    setLoading(false);
   }
 
   const wrapperStyle = "wrapper d-flex my-5";
+  const startScreenStyle = "start d-flex flex-column justify-content-center align-items-center w-100 h-100";
+  const startButtonStyle = "start-btn mt-5 px-5 py-3 shadow";
 
   var total = pizzas.reduce((acc, curr) => acc + (curr.size !== null ? curr.size.price : 0) + curr.toppings.reduce((a, c) => a + c.price, 0), 0);
   total = roundToCents(total);
@@ -102,8 +115,14 @@ function App() {
 
   var total = getTotalPrice(getValidPizzas());
 
+
   return (
     <div className="App">
+        <div className={startScreenStyle + (loading ? "" : " active")}>
+          <h3>Welcome to</h3>
+          <h1>Pizza-Creator !</h1>
+          <div className={startButtonStyle} onClick={onStart}>ORDER</div>
+        </div>
       <Header />
       <div className={wrapperStyle}>
         <Checkout pizzas={getValidPizzas()} total={total}/>
